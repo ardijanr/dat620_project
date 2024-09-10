fb9b1d5c47283d5410c8e21796a5806889750f9e
ardijanr <a.rexhaj@stud.uis.no>
2024-09-10 08:47:41 +0200

    hello world example

---
+++ flake.nix
@@ -0,0 +1,20 @@
+{
+  description = "Development environment";
+  inputs.nixpkgs.url = "github:nixos/nixpkgs";
+  inputs.flake-utils.url = "github:numtide/flake-utils";
+
+  outputs = { self, flake-utils, nixpkgs }:
+    flake-utils.lib.eachDefaultSystem (
+      system:
+      let
+        pkgs = nixpkgs.legacyPackages.${system};
+      in
+      {
+        devShell = pkgs.mkShell {
+          buildInputs = with pkgs; [
+            pkgs.python311Packages.flask
+          ];
+        };
+      }
+    );
+}
