2655a76c73650bcf9aff811b7c33b8a6c230e04d
ardijanr <a.rexhaj@stud.uis.no>
2024-09-17 11:38:18 +0200

    work in progress

--- flake.nix
+++ flake.nix
@@ -13,6 +13,25 @@
         devShell = pkgs.mkShell {
           buildInputs = with pkgs; [
             pkgs.python311Packages.flask
+            pkgs.python311Packages.torchvision
+            pkgs.python311Packages.torchaudio
+            pkgs.python311Packages.torch
+            python311Packages.sentencepiece
+            python311Packages.accelerate
+            python311Packages.transformers
+
+            (
+              pkgs.python311Packages.buildPythonPackage rec {
+                  pname = "mlc-ai-nightly";
+                  version = "0.15.1";
+                  format = "wheel";
+                  src = fetchurl {
+                    sha256 = "sha256-/CNKbs2SBoJscEiAxECCMqjUH1mqt4cvjvwwGhcjLT8=";
+                    url = "https://github.com/mlc-ai/package/releases/download/v0.9.dev0/mlc_ai-0.15.1-cp310-cp310-macosx_10_15_x86_64.whl";
+                  };
+                  buildInputs = [  ];
+                }
+              )
           ];
         };
       }
